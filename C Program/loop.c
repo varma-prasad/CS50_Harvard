f7c81ca8c553d70adc7ca90e2b7d0d7c2fe8f9ad
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-10 19:27:10 +0530

    For & While loop

---
+++ C Program/loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	while (i < 3)
+	{
+		printf("meow \n");
+		i ++;
+	}	
+	printf("--------------------------------\n");
+
+	int j = 3;
+	while (j > 0)
+	{
+		printf("meow \n");
+		j --;
+	}	
+printf("--------------------------------\n");
+
+	for (int k = 0; k < 3; k++ )
+	{
+		printf("meow \n");
+	}
+}
