0d71178d676c84fa6dad45c5f8c76e3c71c8ed3e
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-14 12:18:09 +0530

    While loop

---
+++ Python/loops.py
@@ -0,0 +1,18 @@
+def main():
+	n = int(input("Height: "))
+	for i in range(height(n)):
+		print("Meow") 
+
+def height(n):
+	while True:
+		try:
+			if n>0:
+
+		except ValueError:
+			print("wrong input")
+	return n	
+
+main()	
+
+'''there is no do while loop  in python(it checks the condition and does the iteration). 
+	this type of setting can be achieved by using while True condition'''
