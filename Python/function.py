b4fca1a4e170cf5a12140aa6ba9c18294c63ca7b
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-14 12:27:19 +0530

    Functions

---
+++ Python/function.py
@@ -0,0 +1,15 @@
+def main():
+	for i in range(3):
+		meow()
+	add(5,6)	
+
+def meow():
+	print("Meow")
+
+def add(a,b):
+	print("The sum is:",a+b)
+
+main() 	
+
+''' in python function always has a return value
+     you need not specify type for return and arguments'''
